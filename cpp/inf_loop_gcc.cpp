a0d876f1ab178903b5f48c5ef00e831e01fc9c4f
Xiaoming Mo <mo_xiao_ming@yahoo.com>
2020-05-06 18:18:38 +0800

    update

--- cpp/inf_loop_gcc.cpp
+++ cpp/inf_loop_gcc.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
 int main() {
-    for (int i{0}; i < 200; i++) {
-        std::cout << i << ' ' << i * 20'000'000 << std::endl;
-    }
+  for (int i{0}; i < 200; i++) {
+    std::cout << i << ' ' << i * 20'000'000 << std::endl;
+  }
+  return 0;
 }
