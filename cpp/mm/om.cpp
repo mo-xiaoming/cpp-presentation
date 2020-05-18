cacfb00188fef70e50efb03baaf2155b2668d162
Xiaoming Mo <mo_xiao_ming@yahoo.com>
2020-05-18 21:46:01 +0800

    update

--- cpp/mm/om.cpp
+++ cpp/mm/om.cpp
@@ -1,13 +1,17 @@
 #include <algorithm>
 #include <cstring>
 #include <iostream>
+#include <new>
 
 int main() {
   for (int i = 0; i < 1024 * 1024; ++i) {
-    auto p = new int[1024 * 1024];
-#if 0
-    std::fill_n(p, 1024 * 1024, 3);
-#endif
+    try {
+      auto p = new int[1024 * 1024];
+      std::fill_n(p, 1024 * 1024, 3);
+    } catch (std::bad_alloc const &e) {
+      std::cerr << e.what() << '\n';
+      exit(1);
+    }
     std::cerr << i * sizeof(int) << "MB allocated\n";
   }
 }
