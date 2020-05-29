8f4f9a4f23b3f0141344a50a3f908161628a6f5a
Xiaoming Mo <mo_xiao_ming@yahoo.com>
2020-05-29 10:59:22 +0800

    update

---
+++ cpp/mm/handler.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <new>
+
+void handler() {
+  std::cout << "Memory allocation failed, terminating\n";
+  std::set_new_handler(nullptr);
+}
+
+int main() {
+  std::set_new_handler(handler);
+  try {
+    while (true) {
+      new int[100'000'000ul];
+    }
+  } catch (const std::bad_alloc &e) {
+    std::cout << e.what() << '\n';
+  }
+}
