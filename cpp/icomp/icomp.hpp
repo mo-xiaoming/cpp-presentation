10777c00ecca2ac9cae7c759494c2c619268a53c
Mo Xiaoming <mo_xiao_ming@yahoo.com>
2020-09-05 14:11:04 +0800

    upate

--- cpp/icomp/icomp.hpp
+++ cpp/icomp/icomp.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
+#include <cctype>
 #include <string_view>
 
-constexpr int icmp2(std::string_view a, std::string_view b) noexcept {
-	if (a.size() != b.size()) {
-		return a.size() > b.size() ? 1 : -1;
-	}
-	for (auto i=0U; i<a.size(); ++i) {
-		if (a[i] != b[i]) {
-			return a[i] > b[i] ? 1 : -1;
-		}
-	}
-	return 0;
+constexpr auto icmp2(std::string_view a, std::string_view b) noexcept -> int {
+    constexpr auto to_upper = [](unsigned char c) { return std::toupper(c); };
+
+    if (a.size() != b.size()) {
+        return a.size() > b.size() ? 1 : -1;
+    }
+    for (auto i = 0U; i < a.size(); ++i) {
+        auto const x = to_upper(a[i]);
+        auto const y = to_upper(b[i]);
+        if (x != y) {
+            return x > y ? 1 : -1;
+        }
+    }
+    return 0;
 }
--- cpp/icomp/test_icomp.cpp
+++ cpp/icomp/test_icomp.cpp
@@ -3,30 +3,34 @@
 #include "icomp.hpp"
 
 TEST(IComp, returns_0_if_compare_two_empty_strings) {
-	EXPECT_EQ(icmp2("", ""), 0);
+    EXPECT_EQ(icmp2("", ""), 0);
 }
 
 TEST(IComp, returns_0_if_two_nonempty_string_equal) {
-	EXPECT_EQ(icmp2("abc", "abc"), 0);
+    EXPECT_EQ(icmp2("abc", "abc"), 0);
+}
+
+TEST(IComp, returns_0_if_two_nonempty_string_iequal) {
+    EXPECT_EQ(icmp2("abc", "aBc"), 0);
 }
 
 TEST(IComp, returns_plus_if_first_string_longer_than_second) {
-	EXPECT_GT(icmp2("12345", "123"), 0);
+    EXPECT_GT(icmp2("12345", "123"), 0);
 }
 
 TEST(IComp, returns_minus_if_first_string_shorter_than_second) {
-	EXPECT_LT(icmp2("123", "12345"), 0);
+    EXPECT_LT(icmp2("123", "12345"), 0);
 }
 
 TEST(IComp, return_plus_if_first_lexically_after_second) {
-	EXPECT_GT(icmp2("aaaaab", "aaaaaa"), 0);
+    EXPECT_GT(icmp2("aaaaab", "aaaaaa"), 0);
 }
 
 TEST(IComp, return_minus_if_first_lexically_before_second) {
-	EXPECT_LT(icmp2("aaaaaa", "aaaaab"), 0);
+    EXPECT_LT(icmp2("aaaaaa", "aaaaab"), 0);
 }
 
 int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
