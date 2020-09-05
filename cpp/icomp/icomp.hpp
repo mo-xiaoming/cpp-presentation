7508cab951995401a3a8dc8a7703bd7311694fef
Mo Xiaoming <mo_xiao_ming@yahoo.com>
2020-09-05 18:23:49 +0800

    update

--- cpp/icomp/icomp.hpp
+++ cpp/icomp/icomp.hpp
@@ -10,10 +10,15 @@ constexpr auto icmp2(std::string_view a, std::string_view b) noexcept -> int {
         return a.size() > b.size() ? 1 : -1;
     }
     for (auto i = 0U; i < a.size(); ++i) {
-        auto const x = to_upper(a[i]);
-        auto const y = to_upper(b[i]);
+        auto const m = a[i];
+        auto const n = b[i];
+        if (m == n) {
+            continue;
+        }
+        auto const x = to_upper(m);
+        auto const y = to_upper(n);
         if (x != y) {
-            return x > y ? 1 : -1;
+            return x - y;
         }
     }
     return 0;
