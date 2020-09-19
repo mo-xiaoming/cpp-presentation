00efd43177c882e4127cd255dfbf6f20ba593a93
Mo Xiaoming <mo_xiao_ming@yahoo.com>
2020-09-19 18:26:36 +0800

    update

--- cpp/singleton/test_foo.cpp
+++ cpp/singleton/test_foo.cpp
@@ -8,7 +8,10 @@ struct Mock_logger {
     static void set_level(logging::Level /*unused*/) {}
 };
 
+template int foo<Mock_logger>(int);
+
 TEST_CASE("test foo") {
+    foo(0);
     auto const f = [](int n) { return foo<Mock_logger>(n); };
     CHECK(f(11) == 1);   // NOLINT
     CHECK(f(-11) == -1); // NOLINT
