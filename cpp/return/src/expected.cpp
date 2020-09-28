d66e9e7852f6db18d784f983caa7355c1d27fd94
Mo Xiaoming <mo_xiao_ming@yahoo.com>
2020-09-28 18:20:07 +0800

    update

--- cpp/return/src/expected.cpp
+++ cpp/return/src/expected.cpp
@@ -1,3 +1,5 @@
-//
-// Created by mx on 2020/9/27.
-//
+#include <tl/expected.h>
+
+#include <spdlog/spdlog.hpp>
+
+int main() {}
--- cpp/return/src/optional.cpp
+++ cpp/return/src/optional.cpp
@@ -1,20 +1,22 @@
 #include <optional>
 #include <variant>
 
-#include <spdlog/spdlog.h>
+#include <spdlog/spdlog.hpp>
 
 constexpr auto fail_value = 1;
 
 static std::optional<int> foo2(int i) { return i != fail_value ? std::optional(3) : std::nullopt; }
 
 enum class Error_code : std::uint8_t { error1, error2 };
-template <> struct [[maybe_unused]] fmt::formatter<Error_code> : formatter<std::string_view> {
-    template <typename Format_context> auto format(Error_code code, Format_context& ctx) {
-        static constexpr std::array<std::string_view, 3> codes = {"error1", "error2"};
-        auto const s = codes[std::underlying_type_t<Error_code>(code)]; // NOLINT
-        return formatter<std::string_view>::format(s, ctx);
-    }
-};
+template <>
+struct [[maybe_unused]] fmt::formatter<Error_code>
+    : formatter<std::string_view>{template <typename Format_context> auto format(Error_code code, Format_context& ctx){
+          static constexpr std::array<std::string_view, 3> codes = {"error1", "error2"};
+auto const s = codes[std::underlying_type_t<Error_code>(code)]; // NOLINT
+return formatter<std::string_view>::format(s, ctx);
+}
+}
+;
 
 static std::variant<int, Error_code> foo3(int i) {
     if (i != fail_value) {
